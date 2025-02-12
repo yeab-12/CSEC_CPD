f38b5f34d41af6ea5f9019bcafc813698cf52a86
yeab-12 <yeabsiragetachew613@gmail.com>
2025-02-12 03:55:26 -0800

    Create stones on the table.cpp

---
+++ stones on the table.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;    
+    int count = 0;    
+    // Loop through the stones and check adjacent ones
+    for (int i = 1; i < n; i++) {
+        if (s[i] == s[i - 1]) {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
